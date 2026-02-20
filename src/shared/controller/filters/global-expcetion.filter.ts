f19bcc4cef2317772646ebb31418eb4b415d5906
diegourielvr <diegourielvr@gmail.com>
2026-02-20 10:28:55 -0600

    feat: Agrega manejo de HttpException en el filtro global para capturar las excepciones de del validador de dtos

--- src/shared/controller/filters/global-expcetion.filter.ts
+++ src/shared/controller/filters/global-expcetion.filter.ts
@@ -2,6 +2,7 @@ import {
   ArgumentsHost,
   Catch,
   ExceptionFilter,
+  HttpException,
   HttpStatus,
 } from '@nestjs/common';
 import { BusinessRuleException } from '@shared/domain/exceptions/business-rule.exception';
@@ -9,7 +10,7 @@ import { EntityNotFoundException } from '@shared/domain/exceptions/entity-not-fo
 
 @Catch()
 export class GlobalExceptionFilter implements ExceptionFilter {
-  catch(exception: any, host: ArgumentsHost) {
+  catch(exception: unknown, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse();
 
@@ -17,15 +18,21 @@ export class GlobalExceptionFilter implements ExceptionFilter {
     let message = 'Error interno del servidor';
     let code = 'INTERNAL_ERROR';
 
+    if (exception instanceof HttpException) {
+      status = exception.getStatus();
+      const responseBody = exception.getResponse();
+      message = (responseBody as any).message || responseBody;
+      code = 'VALIDATION_ERROR';
+    }
     // Manejo de tus excepciones de dominio genéricas
     if (exception instanceof EntityNotFoundException) {
       status = HttpStatus.NOT_FOUND; // 404
       message = exception.message;
       code = 'ENTITY_NOT_FOUND';
     } else if (exception instanceof BusinessRuleException) {
-      status = HttpStatus.CONFLICT; // 409
+      status = HttpStatus.BAD_REQUEST; // 400
       message = exception.message;
-      code = exception.code || 'LOGIC_CONFLICT';
+      code = exception.code || 'BAD_REQUEST';
     }
 
     response.status(status).json({
