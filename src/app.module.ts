069470af402df3f10fe1e6ff40c1473f21a3425f
diegourielvr <diegourielvr@gmail.com>
2026-03-06 23:53:29 -0600

    config: Agrega configuracion para manejar eventos

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { CatalogoModule } from '@catalogo/catalogo.module';
 import { OrdenesModule } from '@ordenes/ordenes.module';
 import { VentaModule } from '@ventas/venta.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
-  imports: [CatalogoModule, OrdenesModule, VentaModule],
+  imports: [
+    EventEmitterModule.forRoot(), // Habilita el motor de eventos globalmente
+    CatalogoModule,
+    OrdenesModule,
+    VentaModule,
+  ],
   controllers: [],
   providers: [],
 })
