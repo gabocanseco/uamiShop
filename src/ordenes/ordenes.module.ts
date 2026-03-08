d2974e8479bc7d9871a9ef1da665a25e017d0ef6
diegourielvr <diegourielvr@gmail.com>
2026-03-07 19:16:12 -0600

    feat: Agrega listeners y service al ordenes module

--- src/ordenes/ordenes.module.ts
+++ src/ordenes/ordenes.module.ts
@@ -4,6 +4,10 @@ import { OrdenController } from '@ordenes/controller/orden.controller';
 import { OrdenInMemoryRepository } from '@ordenes/repository/orden-in-memory.repository';
 import { VentaModule } from '@ventas/venta.module';
 import { CarritoService } from '@ventas/service/carrito.service';
+import { ProductoCompradoListener } from '@catalogo/listeners/producto-comprado.listener';
+import { ProductoEstadisticasService } from '@catalogo/service/producto-estadisticas.service';
+import { ProductoEstadisticasInMemoryRepository } from '@catalogo/repository/producto-estadisticas-in-memory-repository';
+import { ProductoAgregadoAlCarritoListener } from '@catalogo/listeners/producto-agregado-al-carrito.listener';
 
 @Module({
   imports: [VentaModule],
@@ -17,6 +21,13 @@ import { CarritoService } from '@ventas/service/carrito.service';
       provide: 'VentasApi',
       useExisting: CarritoService, // Usar la instancia existente de CarritoService como VentasApi
     },
+    ProductoEstadisticasService,
+    {
+      provide: 'IProductoEstadisticasRepository',
+      useClass: ProductoEstadisticasInMemoryRepository,
+    },
+    ProductoCompradoListener,
+    ProductoAgregadoAlCarritoListener,
   ],
   controllers: [OrdenController],
 })
