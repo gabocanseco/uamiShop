70984795a64b28605f01bf9afe5855df6bb59dba
diegourielvr <diegourielvr@gmail.com>
2026-03-15 17:53:13 -0600

    fix: Agrega entidades al ventas module

--- src/ventas/venta.module.ts
+++ src/ventas/venta.module.ts
@@ -3,12 +3,16 @@ import { CarritoController } from '@ventas/controller/carrito.controller';
 import { CarritoService } from '@ventas/service/carrito.service';
 import { CarritoInMemoryRepository } from './repository/carrito-in-memory.repository';
 import { OrdenCreadaListener } from './listeners/orden-creada.listener';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CarritoOrmEntity } from './infrastructure/entities/carrito-orm.entity';
+import { ItemCarritoOrmEntity } from './infrastructure/entities/item-carrito-orm.entity';
 
 /**
  * Módulo de Ventas
  * Gestiona todas las operaciones relacionadas con ventas
  */
 @Module({
+  imports: [TypeOrmModule.forFeature([CarritoOrmEntity, ItemCarritoOrmEntity])],
   controllers: [CarritoController],
   providers: [
     CarritoService,
@@ -20,4 +24,4 @@ import { OrdenCreadaListener } from './listeners/orden-creada.listener';
   ],
   exports: [CarritoService, 'ICarritoRepository'],
 })
-export class VentaModule { }
+export class VentaModule {}
