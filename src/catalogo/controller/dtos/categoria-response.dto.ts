0cfa195b59ab224b27afc285410e6b7fa5cde5f5
diegourielvr <diegourielvr@gmail.com>
2026-03-21 00:25:18 -0600

    fix: Agrega apiProperty a categoriaResponseDto

--- src/catalogo/controller/dtos/categoria-response.dto.ts
+++ src/catalogo/controller/dtos/categoria-response.dto.ts
@@ -6,7 +6,22 @@ export class CategoriaResponseDto {
     format: 'uuid',
   })
   id!: string;
+  @ApiProperty({
+    description: 'Nombre de la categoría',
+    example: 'Electrónica',
+  })
   nombre!: string;
+  @ApiProperty({
+    description: 'Descripción de la categoría',
+    example:
+      'Categoría que incluye productos electrónicos como teléfonos, computadoras, etc.',
+  })
   descripcion!: string;
+  @ApiProperty({
+    description: 'ID de la categoría padre (si existe)',
+    example: '550e8400-e29b-41d4-a716-446655440000',
+    format: 'uuid',
+    required: false,
+  })
   categoriaPadreId?: string;
 }
