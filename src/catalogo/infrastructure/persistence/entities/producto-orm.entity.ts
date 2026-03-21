3680cf31775b483ab7280f7c1608facebbadf2b3
diegourielvr <diegourielvr@gmail.com>
2026-03-21 00:05:55 -0600

    chore: Formatea producto orm entity

--- src/catalogo/infrastructure/persistence/entities/producto-orm.entity.ts
+++ src/catalogo/infrastructure/persistence/entities/producto-orm.entity.ts
@@ -3,27 +3,27 @@ import { MoneyOrmEmbeddable } from '@shared/infrastructure/persistance/embeddabl
 
 @Entity('productos')
 export class ProductoOrmEntity {
-    @PrimaryColumn('varchar', { name: 'id', length: 36 })
-    id!: string;
+  @PrimaryColumn('varchar', { name: 'id', length: 36 })
+  id!: string;
 
-    @Column('varchar', { name: 'nombre', length: 100 })
-    nombre!: string;
+  @Column('varchar', { name: 'nombre', length: 100 })
+  nombre!: string;
 
-    @Column('varchar', { name: 'descripcion', length: 500 })
-    descripcion!: string;
+  @Column('varchar', { name: 'descripcion', length: 500 })
+  descripcion!: string;
 
-    @Column(() => MoneyOrmEmbeddable, { prefix: 'precio_' })
-    precio!: MoneyOrmEmbeddable;
+  @Column(() => MoneyOrmEmbeddable, { prefix: 'precio_' })
+  precio!: MoneyOrmEmbeddable;
 
-    @Column('varchar', { name: 'categoria_id', length: 36 })
-    categoriaId!: string;
+  @Column('varchar', { name: 'categoria_id', length: 36 })
+  categoriaId!: string;
 
-    @Column('boolean', { name: 'disponible', default: false })
-    disponible!: boolean;
+  @Column('boolean', { name: 'disponible', default: false })
+  disponible!: boolean;
 
-    @Column('datetime', { name: 'fecha_creacion' })
-    fechaCreacion!: Date;
+  @Column('datetime', { name: 'fecha_creacion' })
+  fechaCreacion!: Date;
 
-    @Column('json', { name: 'imagenes', nullable: true })
-    imagenes!: { id: string; url: string; alt: string; orden: number }[];
+  @Column('json', { name: 'imagenes', nullable: true })
+  imagenes!: { id: string; url: string; alt: string; orden: number }[];
 }
